8d51ea092f322a4da0ac30157dc4de566266f94a
jackbowen <jackbowen92@gmail.com>
2018-09-25 04:09:43 -0700

    Start of exploded axon view

---
+++ Enclosure/ExplodedSketch/ExplodedSketch.js
@@ -0,0 +1,25 @@
+function preload() {
+  img0 = loadImage('assets/EnclosureBottom.png');
+  img1 = loadImage('assets/Rpi.png');
+  img2 = loadImage('assets/Spacers.png');
+  img3 = loadImage('assets/PCBBack.png');
+  img4 = loadImage('assets/EnclosureTopFront.png');
+  img5 = loadImage('assets/PCBFront.png');
+  img6 = loadImage('assets/EnclosureTopBack.png');
+}
+
+function setup() {
+  createCanvas(800, 900);
+  background(200, 100, 150);
+  image(img0, 0, 0);
+  image(img1, 0, 0);
+  image(img2, 0, 0);
+  image(img3, 0, 0);
+  image(img4, 0, 0);
+  image(img5, 0, 0);
+  image(img6, 0, 0);
+}
+
+function draw() {
+  
+}
